6fe1a764c121a9656f04bb2930f972f06062cc95
Kelly Becker <kelly.lynn.becker@gmail.com>
2020-01-08 14:28:23 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,9 @@ def square_array(array)
   new_array = [] #the new array to put squared numbers
   count = 0
   
-  while count < array.size do # while the count is less than the size of the array
-    new_array << (array[count] **2)
-    count = count + 1
+  while count < array.size do 
+    new_array << array[count] **2
+    count = count += 1
   end
   new_array
 
