6e4b4aa36d715f3b2ebd098cc667b04eb8193d91
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-09 18:09:12 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,19 +1,33 @@
 def square_array(array)
   sq_arr = []
-  count = 0 
-  while count < array.length do
-    sq_num = array[count] ** 2
-    sq_arr << sq_num
-    count += 1
+  array.length.times do |x|
+    sq_nums = array[x]*array[x]
+    sq_arr << sq_nums
   end
-  return sq_arr
-end
+  sq_arr
+end 
 
 
 
-# *** ALTERNATIVE ***
+
+
+
+
+# # *** ALTERNATIVE ***
 # def square_array(array)
 #   array.map! { |n| n**2 } 
 #   return array 
 # end
 
+
+# # *** SECOND ALTERNATIVE ***
+# def square_array(array)
+#   sq_arr = []
+#   count = 0 
+#   while count < array.length do
+#     sq_num = array[count] ** 2
+#     sq_arr << sq_num
+#     count += 1
+#   end
+#   return sq_arr
+# end
